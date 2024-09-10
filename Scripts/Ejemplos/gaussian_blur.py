7a3f86d2c33ea384e5912d4bae0cdb31faba4343
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2024-09-10 09:40:45 -0600

    Leer argumentos del archivo config.txt

--- Scripts/Ejemplos/gaussian_blur.py
+++ Scripts/Ejemplos/gaussian_blur.py
@@ -10,6 +10,16 @@ import matplotlib.pyplot as plt # Mostrar y trabajar con imagenes
 import math               
 from convolution import convolution 
  
+# Leer argumentos del archivo de texto
+def read_config_file(config_path) -> list:
+    with open(config_path, 'r') as file:    
+
+        # Leer las lineas independientes como una lista de argumentos
+        lineas = file.read().splitlines()
+        args = []
+        for line in lineas:
+            args += line.split() # Separar cada lista por espacios, agregarlo a la lista
+        return args
  
 # Calcula la distribucion normal (gausiana) con los argumentos dados
 def dnorm(x, mu, sd):
@@ -34,6 +44,7 @@ def gaussian_kernel(size, sigma=1, verbose=False):
  
     # Mostrar el kernel en una grafica con matplotlib
     if verbose:
+        plt.figure('Figure 1')
         plt.imshow(kernel_2D, interpolation='none', cmap='gray')
         plt.title("Kernel ( {}X{} )".format(size, size))
         plt.show()
@@ -51,13 +62,16 @@ def gaussian_blur(image, kernel_size, verbose=False):
 
 # Main (previene la ejecucion del codigo cuando se importa desde otro archivo) 
 if __name__ == '__main__':
+    # Leer archivo de argumentos
+    config_args = read_config_file('config.txt')
+
     # Objeto para leer los argumentos
     ap = argparse.ArgumentParser()
     
-    # Argumento requerido de la imagen
+    # Argumentos requeridos de la imagen
     ap.add_argument("-i", "--image", required=True, help="Path to the image")
     ap.add_argument("-b", "--blur",type=int, help="Gaussian blur", default = 9)
-    args = vars(ap.parse_args())
+    args = vars(ap.parse_args(config_args))
  
     # Cargar la imagen con el nombre dado 
     image = cv2.imread(args["image"])
